f281056a27ecedaa1a4abf1c7784c1648a1c6d40
kallyous <kallyous@gmail.com>
2019-08-09 18:40:36 -0300

    Implementa criptografar com chave pública

--- cryptofucked.py
+++ cryptofucked.py
@@ -5,7 +5,22 @@ import os
 def genPubKey(p, q):
     e = (p-1)*(q-1)
     n = p*q
-    return n, e
+    return e, n
+
+
+# C = f(M) = M^e mod n
+def encodeChar(char, e, n):
+    return (ord(char) ** e) % n
+
+
+def encrypt(msg, e, n):
+    size = len(msg)
+    ncrptd = []
+    i = 0
+    while(i < size):
+        ncrptd.append(encodeChar(msg[i], e, n))
+        i = i + 1
+    return ncrptd
 
 
 if __name__ == '__main__':
@@ -18,11 +33,18 @@ if __name__ == '__main__':
         p, q = str.split(' ')
         p = int(p)
         q = int(q)
-        n, e = genPubKey(p, q)
+        e, n = genPubKey(p, q)
         print('Chave privada:', p, q)
-        print('Chave pública:', n, e)
+        print('Chave pública:', e, n)
     elif esc == '2':
         print('Criptografar...')
+        str = input('Entre os dois números da chave pública, e, n: ')
+        e, n = str.split(' ')
+        e = int(e)
+        n = int(n)
+        mensagem = input('Digite a mensagem a ser criptografada:\n')
+        mens_enc = encrypt(mensagem, e, n)
+        print('Resultado:\n', mens_enc)
     elif esc == '3':
         print('Descriptografar...')
     else:
